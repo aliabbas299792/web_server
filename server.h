b402e3c01466859cd4f2dbf48a84b220c01f8680
Ali Abbas <aliabbas.299792@gmail.com>
2021-01-02 12:59:15 +0100

    At this point, I don't think you can add in TLS with io_uring without multithreads, so I will just rely on reverse proxy for TLS instead

--- server.h
+++ server.h
@@ -12,6 +12,8 @@
 #include <sys/ioctl.h> //ioctl
 #include <sys/syscall.h> //syscall stuff parameters (as in like __NR_io_uring_enter/__NR_io_uring_setup)
 #include <sys/mman.h> //for mmap
+#include <sys/eventfd.h>
+#include <poll.h>
 
 #include <liburing.h> //for liburing
 
@@ -19,9 +21,11 @@
 
 #include <iostream> //for string and iostream stuff
 
+#include <thread>
+
 #define BACKLOG 10 //max number of connections pending acceptance
 #define READ_SIZE 8192 //how much one read request should read
-#define PORT 8000
+#define PORT 443
 #define QUEUE_DEPTH 256 //the maximum number of events which can be submitted to the io_uring submission queue ring at once, you can have many more pending requests though
 #define READ_BLOCK_SIZE 8192 //how much to read from a file at once
 
@@ -41,6 +45,7 @@ class server{
   private:
     io_uring ring;
     int listener_fd;
+    int event_fd;
 
     void (*accept_callback)(int client_fd, server *web_server) = nullptr;
     void (*read_callback)(int client_fd, char* buffer, unsigned int length, server *web_server) = nullptr;
@@ -54,8 +59,9 @@ class server{
     //accept callbacks for ACCEPT, READ and WRITE
     server(void (*accept_callback)(int client_fd, server *web_server) = nullptr, void (*read_callback)(int client_fd, char *buffer, unsigned int length, server *web_server) = nullptr, void (*write_callback)(int client_fd, server *web_server) = nullptr);
 
-    int add_read_req(int client_fd); //adds a read request to the io_uring ring
+    int add_read_req(int client_fd, char **buff = nullptr, int size = READ_SIZE); //adds a read request to the io_uring ring
     int add_write_req(int client_fd, char *buffer, unsigned int length); //adds a write request using the provided request structure
+    void read_wait(int client_fd); //for reading stuff
 };
 
 #endif
