4ebfd9d821752cfd6817c927047ab3153bce92ae
Ali Abbas <aliabbas.299792@gmail.com>
2021-05-08 20:15:32 +0200

    Reformated the project a little

--- compile.sh
+++ compile.sh
@@ -1,4 +1,14 @@
-cd src && cmake -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_C_FLAGS_DEBUG="-g -O0" -DCMAKE_CXX_FLAGS_DEBUG="-g -O0" -B ../build && cd ..
+SOURCE_FILES=$(find . -type d \( -path ./build -o -path ./src/vendor \) -prune -false -o \( -name *.cpp -o -name *.tcc -o -name *.h \) | sed -E 's:\.\/src\/(.*):\1:g' | tr '\r\n' ' ')
+# above will go through all of the directories, except those specified, and find all .cpp, .h and .tcc files,
+# and make the output into a space separated string of paths
+cd src
+cmake \
+  -DCMAKE_BUILD_TYPE=DEBUG \
+  -DCMAKE_C_FLAGS_DEBUG="-g -O0" \
+  -DCMAKE_CXX_FLAGS_DEBUG="-g -O0" \
+  -B ../build \
+  -DSOURCE_FILES="$SOURCE_FILES"
+cd ..
 cd build && make
-cp Liburing_Server ../server
+cp webserver ../server
 cd ..
