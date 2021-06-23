ae3c6e67bb4c75c579377bf420309c8cd56a3322
Ali Abbas <aliabbas.299792@gmail.com>
2021-06-23 11:34:09 +0200

    Refactored a bit, updated .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main(){
   signal(SIGINT, sigint_handler); //signal handler for when Ctrl+C is pressed
   signal(SIGPIPE, SIG_IGN); //signal handler for when a connection is closed while writing
 
-  central_web_server(".config");
+  auto webserver = central_web_server(".config");
   
   return 0;
 }
